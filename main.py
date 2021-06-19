e1118eb0e0d3e1607e0374583d4e3993bb493a39
Manasa S M <manasarow2001@gmail.com>
2021-06-20 00:42:53 +0530

    Added all assets

--- main.py
+++ main.py
@@ -1,32 +1,16 @@
 import pygame
 import os
 
-pygame.font.init()
+pygame.init()
 
-WIDTH, HEIGHT = 900, 600
-
+WIDTH, HEIGHT = 800, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-
-WHITE = (255,255,255)
-BLACK = (0,0,0)
-
 pygame.display.set_caption("Fruit Ninja")
 
-BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join('Assets','background.jpg')), (WIDTH, HEIGHT))
-
-GAME_FONT = pygame.font.SysFont('comicsans',30)
-
-button = pygame.Rect(WIDTH//2 - 100, HEIGHT//2  + 60, 200, 50)
+BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join('assets','background.png')), (WIDTH, HEIGHT))
 
 def draw_window():
     WIN.blit(BACKGROUND, (0,0))
-    pygame.display.update()
-
-def draw_splash_screen():
-    WIN.blit(BACKGROUND, (0,0))
-    pygame.draw.rect(WIN, WHITE, button)
-    start_game_text = GAME_FONT.render("Start Game", 1, BLACK)
-    WIN.blit(start_game_text, (WIDTH//2 - 60, HEIGHT//2 + 75))
     pygame.display.update()
 
 def main():
@@ -35,7 +19,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        draw_splash_screen()
+        draw_window()
     pygame.quit()
 
 if __name__ == "__main__":
