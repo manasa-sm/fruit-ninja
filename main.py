e8acf50c84aaf436e8104edf541636cb01f5f484
Manasa S M <manasarow2001@gmail.com>
2021-06-28 01:35:49 +0530

    spwaning of fruit

--- main.py
+++ main.py
@@ -1,26 +1,59 @@
 import pygame
-import os
+import random
+
 
 pygame.init()
+screen = pygame.display.set_mode((800, 500))
+pygame.display.set_caption('fruit-ninja')
+clock = pygame.time.Clock()
 
-WIDTH, HEIGHT = 800, 500
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Fruit Ninja")
 
-BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join('assets','background.png')), (WIDTH, HEIGHT))
+class Fruit:
+    def __init__(self, img):
+        self.img = img
+        self.x = 400
+        self.y = 750
+        self.width = self.img.get_width()
+        self.height = self.img.get_height()
+        self.vel = random.randint(-20, 20)
+        self.gravity = random.randint(-22, -20)
+        self.angle = random.randint(0, 355)
 
-def draw_window():
-    WIN.blit(BACKGROUND, (0,0))
+    def draw(self):
+        screen.blit(pygame.transform.rotate(self.img, self.angle), (self.x, self.y))
+
+    def movement(self):
+        self.x += self.vel
+        self.y += self.gravity
+        self.gravity += 0.35
+        if self.vel > 0:
+            self.vel -= 0.25
+        if self.vel < 0:
+            self.vel += 0.25
+        if self.x + self.width >= 800 or self.x <= 0:
+            self.vel *= -1
+        self.angle = (self.angle + 1) % 360
+
+    def update(self):
+        self.draw()
+        self.movement()
+
+
+Images = [pygame.image.load('Assets/apple.png'), pygame.image.load('Assets/banana.png'), pygame.image.load('Assets/orange.png'), pygame.image.load('Assets/pineapple.png'),pygame.image.load('Assets/watermelon.png')]
+
+Fruits = []
+for i in range(random.randint(2, 3)): #no. of fruits spawned at a time
+    Fruits.append(Fruit(Images[0]))#as of now one type of fruit is spawned
+
+background = pygame.image.load('assets/background.png')
+while True:
+    screen.blit(background, (0, 0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit()
+
+    for fruit in Fruits:
+        fruit.update()
+
     pygame.display.update()
-
-def main():
-    run = True
-    while run:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-        draw_window()
-    pygame.quit()
-
-if __name__ == "__main__":
-    main()
+    clock.tick(50)#speed
